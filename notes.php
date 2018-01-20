3e69b1e42073a222249c29639adfe777ff629deb
Mike Bamber <mikebamber@live.com>
2018-01-20 15:38:38 +0000

    Update notes.php

--- notes.php
+++ notes.php
@@ -3,89 +3,8 @@
 /*
 
 */
-class Entity{
-
-    const ALIVE=1;
-    const DEAD=2;
-    const DECAYED=3;
-    const DECAY_VAL=-50;
-
-    protected $energy;
-    protected $state;
-    protected $position;
-    protected $lastAction;
-    
-  public function __construct(Position $pos ,$energy ){
-  	$this->position = $pos;
-    $this->energy = $energy;
-  }
-  	
-    protected $behaviours = array(
-        0=>array(
-            'conditions'=>array(
-                array('target'=>'SELF|'),
-            ),
-            'action'=>array(),
-        )
-    
-    );
-
-    public function live(){
-
-        // re-evaluate current state
-       // $this->chooseBehaviour();
-
-    }
-    public function reduceEnergy($amount){
-        $this->energy -= $amount;
-        $this->checkState();
-        if($this->decayed()){
-            return $amount - ( self::DECAY_VAL - $this->energy );
-        }else{
-            return $amount
-        }
-    }
-    public function increaseEnergy($amount){
-        $this->energy += $amount;
-        $this->checkState();
-    }
-    private function checkState(){
-
-        if($this->alive()){   
-            if($this->energy < self::DECAY_VAL){       
-                $this->state = self::DECAYED;      
-            }else if($this->energy < 0){       
-                $this->state = self::DEAD;        
-            }     
-        }
-    }
-    public function alive(){ return ($this->state==self::ALIVE) }
-    public function dead(){ return ($this->state==self::DEAD) }
-    public function decayed(){ return ($this->state==self::DECAYED) }
-    public function energy(){ return $this->energy; }
-  	
-  protected function giveEnergy(Entity $recipient){} 
-  protected function takeEnergy(Entity $victim){} 
-  protected function absorbEnergy(){} 
-  protected function moveTo(Position $newPosition){} 
-  
-  protected function split($parts){}
-  protected function birth($perc){}
-  protected function merge(Entity $entity){}
-  	
-}
-
-protected function decideBehaviour(){
 
 
-  foreach($this->behaviours as $behaviour){
-    
-    if($behaviour->perform($this)){
-    	return true;
-    }
-  }
-return false;
-}
 
 class Behaviour{
 
@@ -134,9 +53,14 @@ MOVE(target,world){
   World.moveEntityTo(target);
 }
 SPLIT(target,segmenets){
-	spaces = World.adjacents.unoccupied()
-  if(spaces.count >= (segments-1) ){
-  	
+	spaces = World->adjacents(target->position)->unoccupied()
+  if(spaces->count() >= (segments-1) ){
+  	$energy = $target->energy();
+    $energy_chunk = $energy/$segments;
+    foreach($spaces as $space){
+    	$newEntity = Spawner::create($target->getGenome(),$energy_chunk);
+      World->placeAt($space,$newEntity);
+    }
   }
 }
 BIRTH(target)
@@ -194,67 +118,27 @@ class Position{
   	return $this->y;
   }
 }
-class Condition{
-    
-    protected $world;
-    
-    protected $target;
-    protected $method;
-    protected $operator;
-    protected $value;
-    
-    public function __construct($world, $target, $method, $operator, $value ){
-        
-        $this->world = $world;
-        
-        $this->target = $target;
-        $this->method = $method;
-        $this->operator = $operator;
-        $this->value = $value;
-    }
-    
-    protected function getTargetObject($position){
-        switch($this->target){
-            case 'self':
-                return $this->world->getEntityAt($position);
-                break;
-            case 'neighbour':
-                return $this->world->neighboursOf($position)->random();
-                break;
-            case 'neighbours':
-                return $this->world->neighboursOf($position);
-                
-        }
-    }
-    
-    public function evaluate(Position $position){
-        
-      $target_object = $this->getTargetObject($position);
-      if(!$target_object){
-		return false;
-      }
-      $target_value = call_user_func(array($target_object, $this->method));
-      
-        switch($this->operator){
-            case '=':
-                return ($target_value == $this->value);
-                break;
-            case '>':
-                return ($target_value > $this->value);
-                break;
-            case '>=':
-                return ($target_value >= $this->value);
-                break;
-            case '<':
-                return ($target_value < $this->value);
-                break;
-            case '<=':
-                return ($target_value <= $this->value);
-                break;
-        }
-    }
+
+class World{
+  
+  $positions;
+  $entities;
+  
+  public function getEntities(){ return $this->entities;}
+  public function getPositions(){return $this->positions;}
 }
 
+class God{
 
-
-
+  public function loadWorld(){}
+  public function runWorldTick(){}
+}
+       
+       
+       
+       
+       
+       
+       
+       
+       
