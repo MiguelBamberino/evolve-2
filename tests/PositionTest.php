9f98b9ff328d87858c207081d447039c6733cce3
mikebamber <mikebamber@live.com>
2018-01-25 02:46:23 -0500

    getting class to work

--- tests/PositionTest.php
+++ tests/PositionTest.php
@@ -3,5 +3,8 @@ use PHPUnit\Framework\TestCase;
 
 class PositionTest extends TestCase
 {
-  
+ public function testBasicConstruction(){
+   $pos = new evolve\Position(45,34);
+   $this->assertEqual(79,$pos->x()+$pos->y());
+ } 
 }
