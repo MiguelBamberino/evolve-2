0e1bfe1d72e4440f39e472d659cf312fe92b8880
mikebamber <mikebamber@live.com>
2018-01-28 03:00:12 -0500

    adding position tests

--- src/Position.php
+++ src/Position.php
@@ -1,9 +1,18 @@
 <?php
 namespace evolve;
+
 class Position{
+    
 	protected $x;
 	protected $y;
-
+    protected $occupant;
+    
+    /**
+     * construct a position
+     * @test PositionTest::basicConstruction
+     * @param int $x -> value on the x axis
+     * @param int $y -> value on the y axis
+     */
 	public function __construct($x,$y){
 		$this->x = $x;
 		$this->y = $y;
@@ -13,5 +22,34 @@ class Position{
 	}
 	public function y(){
 		return $this->y;
+	}
+    /**
+     * place an entity at this position
+     * @test PositionTest::testPlace
+     * @param Entity $entity -> what to place
+     * @return boolean success
+     */
+    public function place(Entity $entity){
+        if($this->occupied()){
+            return false;
+        }else{
+            $this->occupant = $entity;
+            return true;
+        }
+    }
+    /** 
+     * is the position occupied ?
+     * @test PositionTest::testClear
+     * return boolean
+     */
+    public function occupied(){
+        return !empty($this->occupant);
+    }
+    /**
+     * remove entity reference fom the position, emptying it
+     * @test PositionTest::testClear
+     */
+    public function clear(){
+		$this->occupant = null;
 	}
 }
--- tests/PositionTest.php
+++ tests/PositionTest.php
@@ -3,10 +3,46 @@ use PHPUnit\Framework\TestCase;
 
 class PositionTest extends TestCase
 {
- public function testBasicConstruction(){
-   
-   $pos = new evolve\Position(45,34);
-   $this->assertEquals(79,$pos->x()+$pos->y());
-   
- } 
+    /**
+     * test can we build a basic position object
+     */
+    public function testBasicConstruction(){
+
+        $pos = new evolve\Position(45,34);
+        $this->assertEquals(79,$pos->x()+$pos->y());
+    } 
+    /**
+     * test we can place an entity at this position
+     * @depends testBasicConstruction
+     */
+    public function testCanPlace(){
+        
+        $pos = new evolve\Position(45,34);
+        $placed = $pos->place( new evolve\entity($pos,100) );
+        $this->assertEquals(true,$placed);
+    }
+    /**
+     * test we can place an entity at this position
+     * @depends testCanPlace
+     */
+    public function testCantPlace(){
+        
+        $pos = new evolve\Position(45,34);
+        $pos = new evolve\Position(46,34);
+        $pos->place( new evolve\entity($pos,100) );
+        $placed = $pos->place( new evolve\entity($pos,100) );
+        $this->assertEquals(false,$placed);
+    }
+    /**
+     * test we can place an entity at this position
+     * @depends testBasicConstruction
+     */
+    public function testClearPlace(){
+        
+        $pos = new evolve\Position(45,34);
+        $pos->place( new evolve\entity($pos,100) );
+        $pos->clear();
+        
+        $this->assertEquals(false,$pos->occupied());
+    }
 }
