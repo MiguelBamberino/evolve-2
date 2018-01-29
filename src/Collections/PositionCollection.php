09f9361a023351b709405ba4f404bf9a580e1857
mikebamber <mikebamber@live.com>
2018-01-29 14:31:08 -0500

    new positionCollection func

--- src/Collections/PositionCollection.php
+++ src/Collections/PositionCollection.php
@@ -14,9 +14,27 @@ class PositionCollection extends AbstractCollection{
             $this->set( $key ,$pos);
         }
     }
+    /**
+     * @param mixed $item -> the position to push on the collection
+     */
+    public function push($item){
+        $key = $this->buildKey($pos);
+        $this->set( $key ,$pos);
+    }
+    /**
+     * build a composite key from x,y values of a position
+     * @param Position $pos -> position to key
+     * @return string $key
+     */
     private function buildKey(Position $pos){
         return $this->buildKeyFromCoord($pos->x(),$pos->y());
     }
+    /**
+     * build a composite key from x,y values of a position
+     * @param int $x -> x coord to key
+     * @param int $y -> y coord to key
+     * @return string $key
+     */
     private function buildKeyFromCoord($x,$y){
         return $x ."-".$y;
     }
@@ -28,9 +46,64 @@ class PositionCollection extends AbstractCollection{
     public function getByCoords($x,$y){
         $this->get( $this->buildKeyFromCoord($x,$y) );
     }
-    public function getAdjacentsOf($x,$y);
-    
-    public function getOccupants();
-    public function getOccupied();
-    public function getUnoccupied();
+    /**
+     * get a collection of all the adjacent positions to
+     * the one passed in
+     * @param Position $pos -> position to use as centre
+     * @return PositionCollection $positions
+     */
+    public function getAdjacentsOf(Position $pos){
+        $x = $pos->x();
+        $y = $pos->y();
+        $search = array();
+        $search[0] = $this->getByCoords($x-1,$y-1);
+        $search[1] = $this->getByCoords($x,$y-1);
+        $search[2] = $this->getByCoords($x+1,$y-1);
+        $search[3] = $this->getByCoords($x-1,$y);
+        $search[4] = $this->getByCoords($x+1,$y);
+        $search[5] = $this->getByCoords($x-1,$y+1);
+        $search[7] = $this->getByCoords($x,$y+1);
+        $search[8] = $this->getByCoords($x+1,$y+1);
+        $positions = new PositionCollection();
+        foreach($search as $s){
+            if($s !== false){
+                $positions->push($s);
+            }
+        }
+        return $positions;
+    }
+    /**
+     * Get a sub collection of the positions that
+     * are occupied.
+     * @return PositionCollection $positions
+     */
+    public function getOccupied(){
+        $positions = new PositionCollection();
+        foreach($this as $key=>$pos){
+            if($pos->occupied()){
+                $positions->push($pos);
+            }
+        }
+        return $positions;
+    }
+    /**
+     * Get a sub collection of the positions that
+     * are not occupied.
+     * @return PositionCollection $positions
+     */
+    public function getUnoccupied(){
+        $positions = new PositionCollection();
+        foreach($this as $key=>$pos){
+            if(!$pos->occupied()){
+                $positions->push($pos);
+            }
+        }
+        return $positions;
+    }
+    /**
+     *
+     */
+    public function getOccupants(){
+        
+    }
 }
